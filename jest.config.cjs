46ffc3ca3982828f5d5e12364e38c433624b4949
Joe Chung <26208574+joechung2008@users.noreply.github.com>
2025-09-25 04:09:02 -0400

    Update react-test-render package and Jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -15,7 +15,4 @@ module.exports = {
     "**/__tests__/**/*.(ts|tsx|js)",
     "**/?(*.)+(spec|test).(ts|tsx|js)",
   ],
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))",
-  ],
 };
